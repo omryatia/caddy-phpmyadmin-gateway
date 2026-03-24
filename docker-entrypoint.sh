3404d1065417190da38e4b5985571c19b060be15
omryatia <omryat@gmail.com>
2026-03-24 23:01:52 +0200

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+if [ -z "${BASIC_AUTH_USER:-}" ]; then
+  echo "BASIC_AUTH_USER is required"
+  exit 1
+fi
+
+if [ -z "${BASIC_AUTH_PASSWORD:-}" ]; then
+  echo "BASIC_AUTH_PASSWORD is required"
+  exit 1
+fi
+
+if [ -z "${PMA_UPSTREAM:-}" ]; then
+  echo "PMA_UPSTREAM is required"
+  exit 1
+fi
+
+export BASIC_AUTH_PASSWORD_HASH="$(caddy hash-password --plaintext "$BASIC_AUTH_PASSWORD")"
+
+envsubst < /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
+
+exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
